9b219deeb50e98cd6d44d74d3acdd5e8ec8a232e
szapata-moffitt <juan.zapata-tamayo@moffitt.org>
2024-12-12 13:00:58 -0500

    feat: create basic structure for highlight body parts based on a data frame

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # shinybody
+
 An Interactive Anatomography Widget for Shiny
+
+# Development and testing
+
+## 1. Basic participants data frame
+
+In the R console create the following structure
+
+```bash
+participants <- data.frame(
+     id = 1:3,
+     gender = c("male", "female", "male"),
+     selected_parts = c(
+         "thyroid_gland, adrenal_gland",
+         "heart, liver",
+         "brain, kidney"
+    )
+)
+```
+
+And then run the `human` command
+```bash
+human(participants)
+```
--- inst/htmlwidgets/human.js
+++ inst/htmlwidgets/human.js
@@ -1,28 +1,52 @@
 HTMLWidgets.widget({
+  name: "human",
 
-  name: 'human',
+  type: "output",
 
-  type: 'output',
-
-  factory: function(el, width, height) {
-
-    // TODO: define shared variables for this instance
-
+  factory: function (el) {
     return {
+      renderValue: function (x) {
+        el.innerHTML = "";
 
-      renderValue: function(x) {
+        x.participants.forEach(function (participant) {
+          const container = document.createElement("div");
+          container.id = `participant-${participant.id}`;
+          container.style.marginBottom = "20px";
 
-        // TODO: code to render the widget, e.g.
-        el.innerHTML = x.svg_text;
+          const svgContainer = document.createElement("div");
+          svgContainer.innerHTML = participant.svg_text;
+          svgContainer.style.border = "1px solid #ccc";
+          svgContainer.style.display = "inline-block";
+          svgContainer.style.verticalAlign = "top";
 
+          participant.selected_ids.forEach(function (selected_id) {
+            const selectedPart = svgContainer.querySelector(`#${selected_id}`);
+            if (selectedPart) {
+              selectedPart.style.fill = "red";
+              selectedPart.style.stroke = "blue";
+            }
+          });
+
+          const infoContainer = document.createElement("div");
+          infoContainer.style.display = "inline-block";
+          infoContainer.style.marginLeft = "10px";
+          infoContainer.style.verticalAlign = "top";
+          infoContainer.innerHTML = `
+            <strong>Participant ID:</strong> ${participant.id}<br>
+            <strong>Gender:</strong> ${participant.gender}<br>
+            <strong>Affected Body Parts:</strong> ${participant.selected_parts}<br>
+          `;
+
+          container.appendChild(svgContainer);
+          container.appendChild(infoContainer);
+
+          el.appendChild(container);
+        });
       },
 
-      resize: function(width, height) {
-
+      resize: function (width, height) {
         // TODO: code to re-render the widget with a new size
-
-      }
-
+      },
     };
-  }
+  },
 });
